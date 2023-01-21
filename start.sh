4a841b3f5be51711245d0b92651e234d14285dbc
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-01-21 19:35:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kdbotz/UrlShortner-AutoFilter-Bot.git /UrlShortner-AutoFilter-Bot
+  git clone https://github.com/SexxyNeo/KD-URL.git /KD-URL
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /UrlShortner-AutoFilter-Bot
+  git clone $UPSTREAM_REPO /KD-URL
 fi
-cd /UrlShortner-AutoFilter-Bot
+cd /KD-URL
 pip3 install -U -r requirements.txt
 echo "Starting Lucifer...."
 python3 bot.py
