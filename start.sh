8ed931186c3867c061487f0b403af950006863fe
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-01-21 21:33:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SexxyNeo/KD-URL.git /KD-URL
+  git clone https://github.com/searchingshiv/KD-URL.git /KD-URL
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /KD-URL
