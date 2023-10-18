acaf09a04cb448bae3add04d008277afab218101
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-10-18 17:02:56 +0530

    Update commands.py

--- LUCIFER/commands.py
+++ LUCIFER/commands.py
@@ -11,7 +11,7 @@ from database.users_chats_db import db
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, RQST_LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, CHNL_LNK, GRP_LNK, SUPPORT_GROUP
 from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
 from database.connections_mdb import active_connection
-import re
+import re, asyncio, os, sys
 import json
 import base64
 logger = logging.getLogger(__name__)
