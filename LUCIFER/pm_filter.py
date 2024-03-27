d7179110cf563dab4d8989e69e2a2532610b5c07
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2024-03-27 12:43:44 +0530

    Update pm_filter.py

--- LUCIFER/pm_filter.py
+++ LUCIFER/pm_filter.py
@@ -1081,20 +1081,13 @@ async def auto_filter(client, msg, spoll=False):
         ]
     elif msg.chat.id in filters.chat(chats=SUPPORT_GROUP): return await message.reply_text(script.SGROUP_TXT.format(message.from_user.mention, total_results, search, temp.U_NAME), disable_web_page_preview=True)
     else:
-        btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"{file.file_name}", 
-                    url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")
-                ),
-                InlineKeyboardButton(
-                    text=f"📂 {get_size(file.file_size)} ➛", 
-                    url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")
-                ),
-            ]
-            for file in files
-        ]
-
+    file_texts = []
+    for file in files:
+        if settings["button"]:
+            file_texts.append(f"📂 {get_size(file.file_size)} ➛ {file.file_name}")
+        else:
+            file_texts.append(f"{file.file_name} - 📂 {get_size(file.file_size)} ➛")
+    message_text = "\n".join(file_texts)
     try:
         if settings['auto_delete']:
             btn.insert(0, 
