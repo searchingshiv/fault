7f970ea9e7343af6e1bcd6626fce0efaa0ed5de6
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2024-04-09 23:15:15 +0530

    Update pm_filter.py

--- LUCIFER/pm_filter.py
+++ LUCIFER/pm_filter.py
@@ -34,9 +34,11 @@ logger.setLevel(logging.ERROR)
 
 BUTTONS = {}
 SPELL_CHECK = {}
+DS_REACT = ["🔥", "❤️", "😍", "⚡️"]
 
-@Client.on_message(filters.private & filters.text & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.private & filters.text & filters.incoming)
+@Client.on_message(filters.private & filters.text & filters.group & filters.incoming & filters.chat & filters.user(AUTH_USERS) if AUTH_USERS else filters.private & filters.text & filters.incoming)
 async def pv_filter(client, message):
+    await message.react(emoji=random.choice(DS_REACT))
     kd = await global_filters(client, message)
     if kd == False:
         await auto_filter(client, message)
@@ -45,7 +47,19 @@ async def pv_filter(client, message):
 async def give_filter(client, message):
     if message.chat.id != SUPPORT_GROUP:
         await global_filters(client, message)
+        await message.react(emoji=random.choice(DS_REACT))
     mf = await manual_filters(client, message)
+# @Client.on_message(filters.private & filters.text & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.private & filters.text & filters.incoming)
+# async def pv_filter(client, message):
+#     kd = await global_filters(client, message)
+#     if kd == False:
+#         await auto_filter(client, message)
+
+# @Client.on_message(filters.group & filters.text & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.group & filters.text & filters.incoming)
+# async def give_filter(client, message):
+#     if message.chat.id != SUPPORT_GROUP:
+#         await global_filters(client, message)
+#     mf = await manual_filters(client, message)
     if mf == False:
         settings = await get_settings(message.chat.id)
         try:
